78420ddfbb322b6f092ecf9955c6ba0b842f8e43
andrewsoncha <65234283+andrewsoncha@users.noreply.github.com>
2024-05-30 17:36:53 +0900

    Added Save Function

--- src/layerArtGenerator/ExportWindow.java
+++ src/layerArtGenerator/ExportWindow.java
@@ -2,10 +2,20 @@ package layerArtGenerator;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -37,7 +47,33 @@ public class ExportWindow extends ImageShowWindow{
 		
 		JLabel picLabel = new JLabel(new ImageIcon(redrawImage()));
 		showPanel.add(picLabel);
-		add(showPanel);
+		add(showPanel, BorderLayout.CENTER);
+		
+		JButton writeButton = new JButton("Write to File");
+		writeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				writeImage(currentImg);
+			}
+		});
+		JPanel lowerPanel = new JPanel();
+		lowerPanel.setLayout(new FlowLayout());
+		lowerPanel.add(writeButton);
+		add(lowerPanel, BorderLayout.SOUTH);
 		setVisible(true);
+	}
+	void writeImage(BufferedImage saveImage) {
+		FileDialog fd = new FileDialog(this, "Choose a file", FileDialog.SAVE);
+		fd.setDirectory("C:\\");
+		fd.setFile("*.jpg;*.png");
+		fd.setVisible(true);
+		String filename = fd.getDirectory()+fd.getFile();
+		System.out.println("filename:"+filename);
+		File file = new File(filename);
+		try {
+			ImageIO.write(saveImage, "png", file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
